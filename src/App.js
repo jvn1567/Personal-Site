ad8c21f35f2be553d83a6f5839df1fdc4b03d319
John Nguyen <jvn1567@gmail.com>
2022-12-12 22:01:49 -0800

    fix overwrite

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import MainNav from './components/MainNav';
 
 function App() {
   return (
-    <BrowserRouter>
-      <MainNav />
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <div>
+      <Home />
+    </div>
   );
 }
 
